a8c864a99624f73d67884b0cc928e6bca6cb0adb
yun6160 <devyun6160@gmail.com>
2021-10-09 23:20:24 +0900

    DB connect

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.listen(8080, function () {
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://yun6160:6160dbs@cluster0.bu5yz.mongodb.net/todoapp?retryWrites=true&w=majority', function (에러, client) {
+  app.listen(8080, function () {
+    console.log('listening on 8080')
+  });
 
-  console.log('listening on 8080')
-});
+})
+
+
 // 여기까지 서버 띄우기 위한 기본 셋팅
 
 
-app.get('/', function(요청, 응답){
+app.get('/', function (요청, 응답) {
   응답.sendFile(__dirname + '/index.html');
   // 슬래시: 홈페이지라는 뜻
 });
@@ -19,12 +26,12 @@ app.get('/', function(요청, 응답){
 // 이때 function(){} 은 함수안의 함수라 콜백함수라고 함 -> 순차적으로 실행하고 싶을때 
 // .get('경로', function(요청내용, 응답할 방법){})
 
-app.get('/write', function(요청, 응답){
+app.get('/write', function (요청, 응답) {
   응답.sendFile(__dirname + '/write.html');
-  
+
 });
 
-app.post('/add', function(요청, 응답){
+app.post('/add', function (요청, 응답) {
   응답.send('전송완료')
   console.log(요청.body)
 });
